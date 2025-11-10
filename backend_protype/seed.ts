1744d10bfd4b33d0116b983991cbc11350ecd7f3
Ben <GRA230096@spengergasse.at>
2025-11-10 10:16:11 +0100

    database_protype_usertableonly

--- backend_protype/seed.ts
+++ backend_protype/seed.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "./prisma/client/client.ts";
+//import {}
 const prisma = new PrismaClient();
 
 const seed_users = [
-  {
-    name: "Testname",
-    email: "Testemail@Testemail.Testemail",
-    password: "Testpassword",
-  },
+  { name: "Ben", email: "ben@example.com", password: "abc123" },
+  { name: "jerk", email: "jerk@example.com", password: "abc123" },
+  { name: "marcel", email: "marcel@example.com", password: "abc123" },
+  { name: "dejan", email: "dejan@example.com", password: "abc123" },
+  { name: "Georg", email: "Georg@example.com", password: "abc123" },
 ];
 
 export async function seed() {
@@ -15,4 +16,9 @@ export async function seed() {
       data: user,
     });
   }
+}
+
+if (import.meta.main) {
+  await seed();
+  console.log("Seeding finished.");
 }
