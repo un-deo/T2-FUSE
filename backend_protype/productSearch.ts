6fd3e6baa19e73b0adff2f746eccc02cb89e7cc2
undeo <dem230091@spengergasse.at>
2026-01-28 10:43:25 +0100

    Implement login handler and update router to support login requests

--- backend_protype/productSearch.ts
+++ backend_protype/productSearch.ts
@@ -45,6 +45,59 @@ async function searchHandler(req: Request): Promise<Response> {
   }
 }
 
+  async function loginHandler(req: Request): Promise<Response> {
+  try {
+    const url = new URL(req.url);
+    const searchTerm = url.searchParams.get("logininfo") ?? "";
+    const pw = url.searchParams.get("pw") ?? "";
+
+    if (searchTerm === "" || pw === "") {
+      return new Response(JSON.stringify([false]), {
+        status: 200,
+        headers: corsHeaders(),
+      });
+    }
+
+    const results = await prisma.user.findMany({
+      where: {
+        OR: [
+          { email: { contains: searchTerm } },
+        ],
+      },
+    });
+
+    if(results.length === 0 || results[0].passwort !== pw){
+      return new Response(JSON.stringify([false]), {
+        status: 200,
+        headers: corsHeaders(),
+      });
+    }
+    if(results[0].email === searchTerm && results[0].passwort === pw){
+      return new Response(JSON.stringify([true]), {
+        status: 200,
+        headers: corsHeaders(),
+      });
+    } else {
+      return new Response(JSON.stringify([false]), {
+        status: 200,
+        headers: corsHeaders(),
+      });
+    }
+
+
+    // return new Response(JSON.stringify(results), {
+    //   status: 200,
+    //   headers: corsHeaders(),
+    // });
+  } catch (err) {
+    console.error("useremailHandler error:", err);
+    return new Response(JSON.stringify({ error: "internal" }), {
+      status: 500,
+      headers: corsHeaders(),
+    });
+  }
+}
+
 async function router(req: Request): Promise<Response> {
   const url = new URL(req.url);
 
@@ -53,8 +106,16 @@ async function router(req: Request): Promise<Response> {
   }
 
   if (url.pathname.startsWith("/api/search")) {
-    return await searchHandler(req);
-  }
+    if (url.searchParams.has("search") ) { 
+      return await searchHandler(req)
+    };
+    if (url.searchParams.has("logininfo"))
+    {
+      return await loginHandler(req)
+    }
+    }
+
+  
 
   return new Response(JSON.stringify({ error: "not_found" }), {
     status: 404,
